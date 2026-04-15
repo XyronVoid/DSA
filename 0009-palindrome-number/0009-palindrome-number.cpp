e06b660a0e01f3201050da214794e21ccadb441c
VoidAxiom <harshkkumaar196@gmail.com>
2026-04-15 23:47:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.32%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative ya last digit 0 (except 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
